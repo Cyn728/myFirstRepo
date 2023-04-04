f053a0322d3be5eeb03ed7247544942675edccac
Cyn <cyn@appledeMacBook-Pro-3.local>
2023-04-04 19:36:53 -0400

    This is my first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.arange(0,100)
+y = x*2
+z = x**2
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+
+axes[0].plot(x, x ** 2, x, np.exp(x))
+axes[0].set_title("Normal scale")
+
+axes[1].plot(x, x ** 2, x, np.exp(x))
+axes[1].set_yscale("log")
+axes[1].set_title("Logarithmic scale (y)");
+
+plt.show()
